19ddd27e1a17e5fa5a403a72463d068807f2aac4
za99405com-ui <za99405.com@gmail.com>
2026-09-20 02:46:36 +0300

    Add shared Express adapter for nested Vercel API routes

---
+++ api/_express.ts
@@ -0,0 +1,9 @@
+import { createServer } from '../server.js';
+
+let appPromise: ReturnType<typeof createServer> | null = null;
+
+export default async function handler(req: any, res: any) {
+  if (!appPromise) appPromise = createServer();
+  const app = await appPromise;
+  return app(req, res);
+}
