cec04453165747e257b66ea563f7cea93ee35dd8
za99405com-ui <za99405.com@gmail.com>
2026-09-20 03:00:27 +0300

    Consolidate Vercel API functions

--- api/payments/config.ts
+++
@@ -1,9 +0,0 @@
-import { createPaymentServer } from '../../payment-server.js';
-
-let appPromise: ReturnType<typeof createPaymentServer> | null = null;
-
-export default async function handler(req: any, res: any) {
-  if (!appPromise) appPromise = createPaymentServer();
-  const app = await appPromise;
-  return app(req, res);
-}
